f542b45812cc1a4f89c1903cfc1b3371e3ec8c68
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2015-12-14 17:02:15 +0100

    Add both controllers

--- hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.controller.js
+++ hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.controller.js
@@ -4,14 +4,29 @@
   angular.module('horizon.dashboard.project.hzrequestspanel')
     .controller('Hzrequestspanelcontroller', Hzrequestspanelcontroller);
 
+  Hzrequestspanelcontroller.$inject = ['$scope', '$mdDialog', '$mdMedia'];
 
-  function Hzrequestspanelcontroller() {
+  function Hzrequestspanelcontroller($scope, $mdDialog, $mdMedia) {
       var ctrl = this;
 
       ctrl.openRequestForm = openRequestForm;
 
-      function openRequestForm(context){
+      ctrl.customFullscreen = $mdMedia('sm');
 
+      function openRequestForm(ev){
+        $mdDialog.show({
+          controller: DialogController,
+          templateUrl: '/static/dashboard/project/hzrequestspanel/view.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true,
+          fullscreen: $mdMedia('sm') && ctrl.customFullscreen
+        });
+        $scope.$watch(function() {
+          return $mdMedia('sm');
+        }, function(sm) {
+          ctrl.customFullscreen = (sm === true);
+        });
       }
   }
 
