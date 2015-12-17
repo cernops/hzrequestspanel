f6fc644785d207557032b53296207a40c8c58cc8
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2015-12-17 15:23:26 +0100

    WIP

--- hzrequestspanel/content/views.py
+++ hzrequestspanel/content/views.py
@@ -1,11 +1,5 @@
 from horizon import views
 
-from openstack_dashboard import usage
-
 class IndexView(views.APIView):
     # A very simple class-based view...
     template_name = 'hzrequestspanel/index.html'
-
-    def get_data(self, request, context, *args, **kwargs):
-        # Add data to the context here...
-        return context
--- hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.controller.js
+++ hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.controller.js
@@ -4,15 +4,37 @@
   angular.module('horizon.dashboard.project.hzrequestspanel')
     .controller('Hzrequestspanelcontroller', Hzrequestspanelcontroller);
 
-  Hzrequestspanelcontroller.$inject = ['$scope', '$mdDialog', '$mdMedia'];
-  DialogController.$inject = ['$mdDialog', '$http'];
+  Hzrequestspanelcontroller.$inject = [
+      '$scope',
+      '$mdDialog',
+      '$mdMedia',
+      'horizon.app.core.openstack-service-api.keystone'];
 
-  function Hzrequestspanelcontroller($scope, $mdDialog, $mdMedia) {
+  DialogController.$inject = [
+      '$scope',
+      '$mdDialog',
+      '$http',
+      'horizon.app.core.openstack-service-api.nova',
+      'horizon.app.core.openstack-service-api.keystone',
+      'horizon.app.core.openstack-service-api.cinder'
+  ];
+
+  function Hzrequestspanelcontroller($scope, $mdDialog, $mdMedia, keystoneAPI) {
       var ctrl = this;
+      ctrl.project_name = '';
 
       ctrl.openRequestForm = openRequestForm;
-
       ctrl.customFullscreen = $mdMedia('sm');
+
+      init();
+
+      function init(){
+           keystoneAPI.getCurrentUserSession().success(onGetCurrentUserSession);
+      }
+
+      function onGetCurrentUserSession(dict){
+          ctrl.project_name = dict['project_name'];
+      }
 
       function openRequestForm(ev){
         $mdDialog.show({
@@ -21,20 +43,140 @@
           parent: angular.element(document.body),
           targetEvent: ev,
           clickOutsideToClose:true,
-          fullscreen: $mdMedia('sm') && ctrl.customFullscreen
+          fullscreen: $mdMedia('sm') && $scope.customFullscreen
         });
         $scope.$watch(function() {
           return $mdMedia('sm');
         }, function(sm) {
-          ctrl.customFullscreen = (sm === true);
+          $scope.customFullscreen = (sm === true);
         });
       }
   }
 
-  function DialogController($mdDialog, $http) {
-      var ctrl = this;
+  function DialogController($scope, $mdDialog, $http, novaAPI, keystoneAPI, cinderAPI) {
 
-      ctrl.reset = function() {
+      $scope.nova_limits = {};
+      $scope.project_id = '';
+      $scope.project_name = '';
+      $scope.volume_types = {'volumes': []};
+      $scope.tenant_absolute_limits = {};
+      $scope.volume_type_list_limits = [];
+
+      init();
+
+      function init(){
+          novaAPI.getLimits().success(onGetNovaLimits);
+          keystoneAPI.getCurrentUserSession().success(onGetCurrentUserSession);
+          cinderAPI.volumeTypeList().success(onVolumeTypeList);
+          cinderAPI.tenantAbsoluteLimits().success(onTenantAbsoluteLimits);
+      }
+
+      function onGetNovaLimits(dict){
+          $scope.nova_limits = dict;
+      }
+
+      function onGetCurrentUserSession(dict){
+          $scope.project_id = dict['project_id'];
+          $scope.project_name = dict['project_name'];
+      }
+
+      function onTenantAbsoluteLimits(dict){
+          $scope.tenant_absolute_limits = dict
+          var l = [];
+          var i = 0;
+          for (var k in $scope.volume_types['volumes']){
+              var id = $scope.volume_types['volumes'][k]['id'];
+              var name = $scope.volume_types['volumes'][k]['name'];
+
+              var total_volumes_key_string = 'total_volumes_' + name;
+              var total_gigabytes_key_string = 'total_gigabytes_' + name;
+              var used_volumes_key_string = 'used_volumes_' + name;
+              var used_gigabytes_key_string = 'used_gigabytes_' + name;
+
+              var total_volumes = $scope.tenant_absolute_limits['items'][total_volumes_key_string];
+              var total_gigabytes = $scope.tenant_absolute_limits['items'][total_gigabytes_key_string];
+              var used_volumes = $scope.tenant_absolute_limits['items'][used_volumes_key_string];
+              var used_gigabytes = $scope.tenant_absolute_limits['items'][used_gigabytes_key_string];
+
+              var d = {'id': id,
+                       'name': name,
+                       'volumes': {
+                           'used': used_volumes,
+                           'total': total_volumes
+                       },
+                       'gigabytes': {
+                           'used': used_gigabytes,
+                           'total': total_gigabytes
+                       }
+              };
+             $scope.volume_type_list_limits[i] = d; i++;
+          }
+      }
+
+      function onVolumeTypeList(list){
+          var len = list['items'].length;
+          var vt = [];
+          for (var i=0; i < len; i++){
+              var d = {'id': list['items'][i]['id'],
+                       'name': list['items'][i]['name'],
+                       'description': list['items'][i]['description']
+              };
+              for (var k in list['items'][i]['extra_specs']){
+                  d[k] = list['items'][i]['extra_specs'][k];
+              }
+              vt[d['id']] = d;
+          }
+          $scope.volume_types = {'volumes': vt};
+      }
+
+      $scope.reset = reset;
+
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+
+      $scope.show_form = showForm;
+      $scope.form_display = false;
+      $scope.resquest_sent = false;
+
+      $scope.messageNewProject = showMessageNewProject;
+      $scope.send_request = sendRequest;
+      $scope.name = 'Standard';
+      $scope.usage = 'default';
+      $scope.hyper = 'Linux';
+      $scope.max_iops = '100';
+      $scope.max_throughput = '80 MB/s';
+
+      $scope.show_volume = showVolume;
+      $scope.volume_descrip = false;
+
+      function showVolume(i){
+        $scope.volume_descrip = true;
+        $scope.name = $scope.volume_types['volumes'][i]['name'];
+        $scope.usage = $scope.volume_types['volumes'][i]['usage'];
+        $scope.hyper = $scope.volume_types['volumes'][i]['hypervisor'];
+        $scope.max_iops = $scope.volume_types['volumes'][i]['max_iops'];
+        $scope.max_throughput = $scope.volume_types['volumes'][i]['max_throughput'];
+
+      }
+
+      function showMessageNewProject(){
+        alert('Do we really want this form?')
+      }
+
+      function sendRequest(){
+        $mdDialog.cancel();
+        showGreenMessage();
+      }
+
+      function showForm(b){
+        $scope.form_display = b;
+      }
+
+      function reset(){
         var compute_hidden_fields = ['instances', 'cores', 'ram'];
         var volume_hidden_fields = ['standard', 'wig-cp1', 'wig-cp01', 'io1', 'cp1', 'cp2', 'cp02'];
         for (var i in compute_hidden_fields){
@@ -52,106 +194,39 @@
             changeBackgroundInputs(volume_hidden_fields[i], 'size');
         }
         document.getElementById('textarea-comments').value = '';
-      };
-      ctrl.cancel = function() {
-        $mdDialog.cancel();
-      };
-      ctrl.answer = function(answer) {
-        $mdDialog.hide(answer);
-      };
+      }
 
-      ctrl.volumes = {'volumes': [
-        {
-            "name": "Standard",
-          "usage": "default",
-          "hypervisor": "Linux",
-          "max_iops": 100,
-          "max_throughput": "80 MB/s"
-        },
-        {
-            "name": "WIG-CP1",
-            "usage": "critical power in Wigner",
-            "hypervisor": "Linux",
-            "max_iops": 100,
-            "max_throughput": "80 MB/s"
-        },
-        {
-            "name": "WIG-CPO1",
-            "usage": "critical power IO intensive in Wigner",
-            "hypervisor": "Linux",
-            "max_iops": 500,
-            "max_throughput": "120 MB/s"
-        },
-        {
-            "name": "IO1",
-            "usage": "  IO intensive",
-            "hypervisor": "Linux",
-            "max_iops": 500,
-            "max_throughput": "120 MB/s"
-        },
-        {
-            "name": "CP1",
-            "usage": "critical power",
-            "hypervisor": "Linux",
-            "max_iops": 100,
-            "max_throughput": "80 MB/s"
-        },
-        {
-            "name": "CP2",
-            "usage": "critical power",
-            "hypervisor": "Windows",
-            "max_iops": 100,
-            "max_throughput": "80 MB/s"
-        },
-        {
-            "name": "CPIO2",
-            "usage": "critical power",
-            "hypervisor": "Linux",
-            "max_iops": 100,
-            "max_throughput": "80 MB/s"
+      function changeNumber(prefix_id, field){
+        changeBackgroundInputs(prefix_id, field)
+
+        if(field == 'number'){
+          var input = document.getElementById(prefix_id + '_' + field);
+          var volume_size = document.getElementById(prefix_id + '_size');
+          if(volume_size){
+            if(parseInt(input.value) > parseInt(volume_size.value)){
+              volume_size.value = input.value;
+            }
+            changeBackgroundInputs(prefix_id, 'size');
+          }
         }
-      ]};
-
-      ctrl.show_form = showForm;
-      ctrl.form_display = false;
-      ctrl.resquest_sent = false;
-
-      ctrl.messageNewProject = showMessageNewProject;
-      ctrl.send_request = sendRequest;
-      ctrl.name = 'Standard';
-      ctrl.usage = 'default';
-      ctrl.hyper = 'Linux';
-      ctrl.max_iops = '100';
-      ctrl.max_throughput = '80 MB/s';
-
-      ctrl.show_volume = showVolume;
-      ctrl.volume_descrip = false;
-
-      ctrl.create_ticket_msg = "gola";
-
-      function showVolume(i, id){
-        ctrl.volume_descrip = true;
-        ctrl.name = ctrl.volumes['volumes'][i]['name'];
-        ctrl.usage = ctrl.volumes['volumes'][i]['usage'];
-        ctrl.hyper = ctrl.volumes['volumes'][i]['hypervisor'];
-        ctrl.max_iops = ctrl.volumes['volumes'][i]['max_iops'];
-        ctrl.max_throughput = ctrl.volumes['volumes'][i]['max_throughput'];
-
       }
 
-      function showMessageNewProject(){
-        alert('Do we really want this form?')
+      function changeBackgroundInputs(prefix_id, field){
+        var input_actual = document.getElementById(prefix_id + '_' + field + '_actual');
+        var input = document.getElementById(prefix_id + '_' + field);
+
+        if(parseInt(input.value) > parseInt(input_actual.value)){
+          input.style.background = '#2CB04B';
+          input.style.color = '#FFFFFF';
+        }else if (parseInt(input.value) < parseInt(input_actual.value)) {
+          input.style.background = '#7DA6B8'; //#006CCF//darkblue
+          input.style.color = '#FFFFFF';
+        }else{
+          input.style.background = '';
+          input.style.color = '#000000';
+        }
       }
 
-      function sendRequest(){
-        $mdDialog.cancel();
-        showGreenMessage();
-      }
-
-      function showForm(b){
-        ctrl.form_display = b;
-      }
     }
-
 
 })();
