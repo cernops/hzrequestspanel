8eb65f56e0869f4c2f44cc77f25830951606ca4b
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2016-01-06 16:04:52 +0100

    Added regex for urls.py

--- hzrequestspanel/api/hzrequests_rest_api.py
+++ hzrequestspanel/api/hzrequests_rest_api.py
@@ -6,10 +6,15 @@ from openstack_dashboard.api.rest import utils as rest_utils
 from openstack_dashboard.api.rest import urls
 
 @urls.register
-class Request(generic.View):
+class HZRequest(generic.View):
     """API for requests.
     """
     url_regex = r'hzrequests/requests/$'
+
+    @rest_utils.ajax()
+    def get(self, request):
+        print "hola"
+        return {}
 
     @rest_utils.ajax(data_required=True)
     def post(self, request):
--- hzrequestspanel/content/urls.py
+++ hzrequestspanel/content/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 
 from hzrequestspanel.content import views
+from hzrequestspanel.api import hzrequests_rest_api
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(hzrequests_rest_api.HZRequest.url_regex, hzrequests_rest_api.HZRequest.as_view(), name='createrequest'),
 )
