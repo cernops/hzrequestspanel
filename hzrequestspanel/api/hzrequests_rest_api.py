f344c0a1b2dcac95c1861f415626322fd64951c7
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2016-01-08 16:48:41 +0100

    Added ccitools call for snowticket creation

--- hzrequestspanel/api/hzrequests_rest_api.py
+++ hzrequestspanel/api/hzrequests_rest_api.py
@@ -1,8 +1,8 @@
 from django.views import generic
 
 from hzrequestspanel.api import hzrequests_service
+from openstack_dashboard.api import cinder
 from openstack_dashboard.api.rest import utils as rest_utils
-
 from openstack_dashboard.api.rest import urls
 
 @urls.register
@@ -13,4 +13,8 @@ class HZRequest(generic.View):
 
     @rest_utils.ajax(data_required=True)
     def post(self, request):
-        return hzrequests_service.create(request.DATA)
+        volume_type_list = cinder.volume_type_list(request)
+        volume_type_name_list = []
+        for vt in volume_type_list:
+            volume_type_name_list.append(vt.name)
+        return hzrequests_service.create(request.DATA, volume_type_name_list)
--- hzrequestspanel/api/hzrequests_service.py
+++ hzrequestspanel/api/hzrequests_service.py
@@ -1,6 +1,42 @@
 import logging
+
+from ccitools.cloud import CloudClient
+from ccitools.servicenow import ServiceNowClient
+
 LOG = logging.getLogger(__name__)
+
+def _create(dict_data, volume_type_name_list):
+    # Service Now data needed
+    sn_user = ''
+    sn_pass = ''
+    sn_instance = 'cerntest'
+
+    # Setup clients
+    snowclient = ServiceNowClient(sn_user, sn_pass, instance=sn_instance)
+    cloud = CloudClient()
+
+    # Extra params needed
+    short_description = "Request change of resource quota for the " \
+                        "Cloud Project: {0}".format(dict_data['projectname'])
+    funtional_element = "Cloud Infrastructure"
+    group = "Cloud Infrastructure 3rd Line Support"
+ 
+    # Create the ticket
+    ticket = snowclient.create_request(short_description, funtional_element,
+                                       assignment_group=group)
+    # Fill the ticket
+    snowclient.create_quota_update(ticket.number, volume_type_name_list, dict_data)
+
+    return ticket.number
+
 def create(dict_data):
     LOG.info("Creating service now ticket with: {0}".format(dict_data))
     # Call Daniel's function
-    return {"status_code": 201, "ticket_id": "INC74616"}
+    ticket_id = ''
+    try {
+       ticket_id = _create(dict_data, volume_type_name_list)
+       return {"ticket_id": ticket_id}
+    } except Exception as e:
+        LOG.error(e.message)
+        return {"error_message": e.message}
+    }
