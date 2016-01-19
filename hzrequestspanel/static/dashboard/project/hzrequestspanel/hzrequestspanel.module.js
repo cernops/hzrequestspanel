c3e46eee28694430871445e7d4325ee59ec35434
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2016-01-19 14:19:02 +0100

    Load ngMaterial module only in plugin web pages

--- hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.module.js
+++ hzrequestspanel/static/dashboard/project/hzrequestspanel/hzrequestspanel.module.js
@@ -1,8 +1,13 @@
 (function() {
   'use strict';
 
+  var deps = [];
+  if (window.location.href.indexOf('dashboard/project/hzrequestspanel/') > -1){
+    deps = ['ngMaterial'];
+  }
+
   angular
-    .module('horizon.dashboard.project.hzrequestspanel', ['ngMaterial'])
+    .module('horizon.dashboard.project.hzrequestspanel', deps)
     .config(config);
 
   config.$inject = [
