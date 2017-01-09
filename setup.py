cd663d423159364d97901690ad4703387e989140
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-01-09 10:23:55 +0100

    New monitoring link

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'hzrequestspanel',
-    version = '1.2',
+    version = '1.4',
     description = 'A plugin for OpenStack Dashboard Panel SNOW requests',
     author='Cloud Infrastructure Team',
     author_email='cloud-infrastructure-3rd-level@cern.ch',
